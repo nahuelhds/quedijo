c71740269ca5248d5f35339c15f91fe1006be779
Nahue <nahuelhds@gmail.com>
2020-06-06 17:13:34 -0300

    Refactor with rate_limit and call quantity calculation

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
 import argparse
 import json
+import random
 import textwrap
+import time
+
 import GetOldTweets3 as got
 
 from dotenv import load_dotenv
 from os import environ
-from expiringdict import ExpiringDict
 from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler
+from tweepy import OAuthHandler, TweepError
 from tweepy import Stream
 from tweepy import API
 
@@ -24,23 +26,53 @@ TRACK = "#quedijo sobre"
 
 
 class ReplyToTweet(StreamListener):
-    def __init__(self, api):
-        self.api = api
-        self.__max_per_user = 5  # max replies per user within an hour
-        self.__rate_limits_per_user = ExpiringDict(
-            max_len=1000, max_age_seconds=(1 * 60)
-        )
+    calls_per_window = 180
+    remaining_calls = 180
+    rate_limit_seconds = 15 * 60
+
+    def __init__(self, twitter_api):
+        self.api = twitter_api
+
+    def wait_rate_limit(self):
+        time.sleep(self.rate_limit_seconds)
+
+    def post_status(self, text, in_reply_to_status_id=None):
+        if self.remaining_calls == 0:
+            self.wait_rate_limit()
+            self.remaining_calls = self.calls_per_window
+
+        self.remaining_calls -= 1
+
+        try:
+            self.wait_like_a_human()
+            status = self.api.update_status(
+                self.build_status(text),
+                in_reply_to_status_id=in_reply_to_status_id,
+                auto_populate_reply_metadata=True,
+            )
+        except TweepError:
+            self.wait_rate_limit()
+            status = self.post_status(text, in_reply_to_status_id)
+
+        return status
+
+    def build_status(self, text):
+        return textwrap.shorten(text, width=280, placeholder="...")
+
+    def wait_like_a_human(self):
+        time.sleep(random.randint(3, 60))
 
     def on_data(self, data):
         if not data:
             return
 
         tweet = json.loads(str(data).strip())
-        retweeted = tweet.get("retweeted")
-        from_self = False  # tweet.get('user', {}).get('id_str','') == ACCOUNT_USER_ID
-        print(tweet)
-
         requesting_user = tweet.get("user", {}).get("screen_name")
+
+        if requesting_user != "nahuelhds":
+            print("no soy yo sino @%s. Omitiendo por ahora..." % requesting_user)
+            return
+
         requesting_tweet_id_str = tweet.get("id_str")
         mentioned_user = (
             tweet.get("entities", {}).get("user_mentions", [])[0].get("screen_name")
@@ -52,49 +84,65 @@ class ReplyToTweet(StreamListener):
             got.manager.TweetCriteria()
             .setQuerySearch("from:%s %s" % (mentioned_user, query))
             .setTopTweets(True)
+            .setMaxTweets(10)
+            .setEmoji("unicode")
         )
         for result in got.manager.TweetManager.getTweets(criteria):
             thread.append(result.permalink)
 
         if len(thread) == 0:
             print("Nothing found. Replying...")
-            last_tweet = self.api.update_status(
-                "No encontré nada al respecto 🤔",
-                in_reply_to_status_id=requesting_tweet_id_str,
-                auto_populate_reply_metadata=True,
+            last_tweet = self.post_status(
+                "No encontré nada al respecto 🤔", requesting_tweet_id_str
+            )
+            print(
+                "Replied:  https://twitter.com/%s/status/%s"
+                % (ACCOUNT_SCREEN_NAME, last_tweet.id_str)
             )
         else:
             print("Creating thread...")
-            text = 'Esto fue lo que dijo @%s sobre "%s"' % (mentioned_user, query)
-            last_tweet = self.api.update_status(build_status(text))
+            text = "Esto fue lo que dijo @%s sobre %s" % (mentioned_user, query)
+            last_tweet = self.post_status(text)
             thread_id_str = last_tweet.id_str
             print(
                 "Thread: https://twitter.com/%s/status/%s"
                 % (ACCOUNT_SCREEN_NAME, thread_id_str)
             )
+
+            print("Replying to request...")
+            reply_tweet = self.post_status(
+                "Te paso lo que voy encontrando en este hilo👇\nhttps://twitter.com/%s/status/%s"
+                % (ACCOUNT_SCREEN_NAME, thread_id_str),
+                requesting_tweet_id_str,
+            )
+            print(
+                "Replied:  https://twitter.com/%s/status/%s"
+                % (ACCOUNT_SCREEN_NAME, last_tweet.id_str)
+            )
             for text in thread:
-                last_tweet = self.api.update_status(
-                    build_status(text),
-                    in_reply_to_status_id=last_tweet.id_str,
-                    auto_populate_reply_metadata=True,
-                )
+                last_tweet = self.post_status(text, last_tweet.id_str)
                 print(
                     "Tweet: https://twitter.com/%s/status/%s"
                     % (ACCOUNT_SCREEN_NAME, thread_id_str)
                 )
 
             print("Replying to request...")
-            self.api.update_status(
+            last_tweet = self.post_status(
                 "https://twitter.com/%s/status/%s"
                 % (ACCOUNT_SCREEN_NAME, thread_id_str),
-                in_reply_to_status_id=requesting_tweet_id_str,
-                auto_populate_reply_metadata=True,
+                requesting_tweet_id_str,
+            )
+            print(
+                "Replied:  https://twitter.com/%s/status/%s"
+                % (ACCOUNT_SCREEN_NAME, last_tweet.id_str)
             )
 
-        print(
-            "Replied:  https://twitter.com/%s/status/%s"
-            % (ACCOUNT_SCREEN_NAME, last_tweet.id_str)
-        )
+            print("Replying to request...")
+            last_tweet = self.post_status("Terminé mi búsqueda 🥳", reply_tweet.id_str,)
+            print(
+                "Replied:  https://twitter.com/%s/status/%s"
+                % (ACCOUNT_SCREEN_NAME, last_tweet.id_str)
+            )
         return
 
     def on_error(self, status):
@@ -123,10 +171,6 @@ def get_auth_link_and_show_token():
     print("\nThese are your access token and secret.\nDO NOT SHARE THEM WITH ANYONE!\n")
     print("ACCESS_TOKEN\n%s\n" % token[0])
     print("ACCESS_TOKEN_SECRET\n%s\n" % token[1])
-
-
-def build_status(text):
-    return textwrap.shorten(text, width=280, placeholder="...")
 
 
 if __name__ == "__main__":
