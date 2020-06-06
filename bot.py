e31eb1176135aeb90526faa2cba60e51fec9f372
Nahue <nahuelhds@gmail.com>
2020-06-06 14:39:01 -0300

    Black formatter

--- bot.py
+++ bot.py
@@ -12,40 +12,46 @@ from tweepy import API
 load_dotenv()
 
 # store Twitter specific credentials
-CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET = environ['CONSUMER_SECRET']
-ACCESS_TOKEN_KEY = environ['ACCESS_TOKEN_KEY']
-ACCESS_TOKEN_SECRET = environ['ACCESS_TOKEN_SECRET']
-ACCOUNT_SCREEN_NAME = 'quedijo__'
-ACCOUNT_USER_ID = '1269034001639059459'
-TRACK = '#quedijo sobre'
+CONSUMER_KEY = environ["CONSUMER_KEY"]
+CONSUMER_SECRET = environ["CONSUMER_SECRET"]
+ACCESS_TOKEN_KEY = environ["ACCESS_TOKEN_KEY"]
+ACCESS_TOKEN_SECRET = environ["ACCESS_TOKEN_SECRET"]
+ACCOUNT_SCREEN_NAME = "quedijo__"
+ACCOUNT_USER_ID = "1269034001639059459"
+TRACK = "#quedijo sobre"
+
 
 class ReplyToTweet(StreamListener):
-
     def __init__(self, api):
         self.api = api
-        self.__max_per_user = 5 # max replies per user within an hour
-        self.__rate_limits_per_user = ExpiringDict(max_len=1000, max_age_seconds=(1 * 60))
+        self.__max_per_user = 5  # max replies per user within an hour
+        self.__rate_limits_per_user = ExpiringDict(
+            max_len=1000, max_age_seconds=(1 * 60)
+        )
 
     def on_data(self, data):
         if not data:
             return
 
         tweet = json.loads(str(data).strip())
-        retweeted = tweet.get('retweeted')
-        from_self = False # tweet.get('user', {}).get('id_str','') == ACCOUNT_USER_ID
+        retweeted = tweet.get("retweeted")
+        from_self = False  # tweet.get('user', {}).get('id_str','') == ACCOUNT_USER_ID
         print(tweet)
 
-        requesting_user = tweet.get('user', {}).get('screen_name')
-        requesting_tweet_id = tweet.get('id_str')
-        mentioned_user = tweet.get("entities", {}).get("user_mentions", [])[0].get('screen_name')
-        query = extract_query_from_tweet(tweet.get('text'), mentioned_user)
+        requesting_user = tweet.get("user", {}).get("screen_name")
+        requesting_tweet_id = tweet.get("id_str")
+        mentioned_user = (
+            tweet.get("entities", {}).get("user_mentions", [])[0].get("screen_name")
+        )
+        query = extract_query_from_tweet(tweet.get("text"), mentioned_user)
 
         thread = ['Esto fue lo que dijo @%s sobre "%s"' % (mentioned_user, query)]
-        for result in Cursor(self.api.search, q='from:%s %s' % (mentioned_user, query)).items():
+        results = Cursor(
+            self.api.search, q="from:%s %s" % (mentioned_user, query)
+        ).items()
+        for result in results:
             print(result)
             thread.append("https://twitter.com/%s/%s" % (mentioned_user, result.id_str))
-
         print(thread)
         # check if we already replied to this user and when
         # rate limit replies per user
@@ -71,12 +77,14 @@ class ReplyToTweet(StreamListener):
     def on_error(self, status):
         print(status)
 
+
 def extract_query_from_tweet(text, mentioned_user):
-    for term in TRACK.split(' '):
+    for term in TRACK.split(" "):
         text = text.replace(term, "")
 
-    text = text.replace('@%s' % mentioned_user, "")
-    return ' '.join(text.split())
+    text = text.replace("@%s" % mentioned_user, "")
+    return " ".join(text.split())
+
 
 def get_auth_link_and_show_token():
     auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -94,7 +102,7 @@ def get_auth_link_and_show_token():
     print("ACCESS_TOKEN_SECRET\n%s\n" % token[1])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Cli options
     parser = argparse.ArgumentParser()
     parser.add_argument("--auth", action="store_true")
@@ -110,4 +118,3 @@ if __name__ == '__main__':
         streamListener = ReplyToTweet(api)
         twitterStream = Stream(auth, streamListener)
         twitterStream.filter(track=[TRACK])
-
